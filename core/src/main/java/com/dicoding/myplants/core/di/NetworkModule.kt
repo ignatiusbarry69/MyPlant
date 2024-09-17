4e296f42845a70d1feb514f633a2caea65e91c55
barry <ignatius.barry@gmail.com>
2024-09-17 13:19:42 +0700

    stop ssl pining first

--- core/src/main/java/com/dicoding/myplants/core/di/NetworkModule.kt
+++ core/src/main/java/com/dicoding/myplants/core/di/NetworkModule.kt
@@ -28,7 +28,7 @@ class NetworkModule {
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
-            .certificatePinner(certificatePinner)
+//            .certificatePinner(certificatePinner)
             .build()
     }
 
